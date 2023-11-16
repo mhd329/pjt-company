2f2c9e712f49359391512f95ceed3bde042867d8
mhd329 <mhd329@gmail.com>
2023-11-17 00:12:39 +0900

    install dotenv

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,3 +6,6 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=os.getenv("DEBUG") == "True")
